ff93551cfa1d182e089cb2716bb81a05abb5fb29
pinch <pinchasmt@gmail.com>
2024-10-01 11:44:07 +0300

    The login page. including listening, sending a registration and entering the bankand and displaying appropriate messages

---
+++ frontend/login/script.js
@@ -0,0 +1,69 @@
+const registerForm = document.getElementById("registerForm")
+const loginForm = document.getElementById ("loginForm")
+
+registerForm.addEventListener("submit", async  (event) => {
+   
+      //מניעת ניקוי הטופס
+    event.preventDefault();
+    const newUser = {
+        name : event.target[0].value,
+        password : event.target[1].value
+    }
+    createuser(newUser); 
+});
+
+loginForm.addEventListener("submit", async  (event) => {
+    //מניעת ניקוי הטופס 
+    event.preventDefault();
+    const loginUser = {
+        name : event.target[0].value,
+        password : event.target[1].value
+    }
+    loginAndcreateToken(loginUser); 
+})
+async function loginAndcreateToken(loginUser) {
+    const response = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginUser),
+    });
+    const data = await response.json();
+    if (data.success) {
+        saveToken(data.token);
+        window.location.href = "play.html";
+    } else {
+        userNotice("שם משתמש או סיסמה אינם תקינים");
+    }
+}
+async function createuser(newUser) {
+    const response = await fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newUser),
+    });
+    const data = await response.json();
+    const answer =""
+    if (data.success) {
+        answer = "המשתמש נוצר בהצלחה"
+    } else {
+        answer = "  חלה שגיאה בהוספת המשתמש"
+    }
+    userNotice(answer);
+}
+async function saveToken(token) {
+    localStorage.setItem("token", token);
+}
+async function userNotice(data) {
+  console.log(data);
+  const notice = document.createElement("div");
+  notice.classList.add("notice");
+  notice.textContent = data;
+  document.body.appendChild(notice);
+  setTimeout(() => {
+    notice.remove();
+  }, 3000);
+}
